6552bb28ae4df56fc6a5208ab506db56842d8871
Aditya Jethani <97025644+okaditya84@users.noreply.github.com>
2023-10-28 08:10:56 +0530

    added one more approach

--- POTDs/GFGPOTDbleakNumbers.py
+++ POTDs/GFGPOTDbleakNumbers.py
@@ -8,3 +8,17 @@ class Solution:
             if e + bin(e).count('1') == n:
                 return 0
         return 1
+
+class Solution:
+	def is_bleak(self, n):
+		# Code here
+        def countSetBits(x):
+            count =0
+            while x:
+                x &= (x-1)
+                count+=1
+            return count
+        for i in range(n):
+            if i+countSetBits(i)==n:
+                return 0
+        return 1
