8f7964a9cc159c8919ede6d35087bc60c5119b4e
Aditya Jethani <97025644+okaditya84@users.noreply.github.com>
2023-11-07 09:04:22 +0530

    added code for 7/11/23

--- POTDs/GFGPOTDsumOfUpperLowerTrainagles.py
+++ POTDs/GFGPOTDsumOfUpperLowerTrainagles.py
@@ -2,5 +2,20 @@ class Solution:
     
     #Function to return sum of upper and lower triangles of a matrix.
     def sumTriangles(self,matrix, n):
-        # code here 
+        upper_sum = 0
+        lower_sum = 0
         
+        #iterating through the matrix.
+        for i in range(n):
+            for j in range(n):
+                
+                #condition to check upper triangle.
+                if i <= j:
+                    upper_sum += matrix[i][j]
+                
+                #condition to check lower triangle.
+                if i >= j:
+                    lower_sum += matrix[i][j]
+        
+        #returning the list of sums.
+        return [upper_sum, lower_sum]
