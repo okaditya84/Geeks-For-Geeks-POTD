dc8c50a9bacdddef00a0cbe260d5c60824713863
Aditya Jethani <97025644+okaditya84@users.noreply.github.com>
2024-02-01 17:12:22 +0530

    test cpp

---
+++ DSA/STL/nicheDekho.cpp
@@ -0,0 +1,17 @@
+//demonstration Fira Code
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=0,b=1;
+    int c=a+b;
+    for(int i=0;i<=10;i++){
+        cout<<c<<endl;
+    }
+    if(a==0 && b==1){
+        cout<<"Salary kitna loge, discuss kar le?";
+    }
+    if(a!=0 || b!=1){
+        cout<<"We'll get back to you.";
+    }    
+}
